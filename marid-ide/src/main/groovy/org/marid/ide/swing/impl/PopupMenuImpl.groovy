7e7421623b3ce009bc59d7da1a95c791ec4003ef
Dmitry Ovchinnikov <d.ovchinnikow@gmail.com>
2013-03-20 21:41:28 +0300

    Moving log item to the tray

--- marid-ide/src/main/groovy/org/marid/ide/swing/impl/PopupMenuImpl.groovy
+++ marid-ide/src/main/groovy/org/marid/ide/swing/impl/PopupMenuImpl.groovy
@@ -41,11 +41,18 @@ class PopupMenuImpl extends PopupMenu implements ActionListener {
         application = app;
         def activateItem = new MenuItem("Show/Hide the main window".ls());
         activateItem.font = font.deriveFont(Font.BOLD);
+        activateItem.actionCommand = "show_hide";
         activateItem.addActionListener(this);
         add(activateItem);
         addSeparator();
-        def items = menuEntries.findAll{it.path.length == 0};
-        for (def entry in items.sort(false, {e1, e2 -> e1.priority <=> e2.priority})) {
+        def showLogItem = new MenuItem("Show log".ls());
+        showLogItem.font = font;
+        showLogItem.actionCommand = "show_log";
+        showLogItem.addActionListener(this);
+        add(showLogItem);
+        addSeparator();
+        def items = menuEntries.findAll { it.path.length == 0 };
+        for (def entry in items.sort(false, { e1, e2 -> e1.priority <=> e2.priority })) {
             switch (entry.type) {
                 case MenuType.ITEM:
                     add(getItem(entry));
@@ -66,14 +73,16 @@ class PopupMenuImpl extends PopupMenu implements ActionListener {
     private def getItem(MenuEntry entry) {
         def action = new MenuActionImpl(entry);
         def item = new MenuItem(action.getValue(Action.NAME) as String);
+        item.font = font;
         item.addActionListener(action);
         item.actionCommand = action.getValue(Action.ACTION_COMMAND_KEY);
         return item;
     }
 
-   private def getCheckedItem(MenuEntry entry) {
+    private def getCheckedItem(MenuEntry entry) {
         def action = new MenuActionImpl(entry);
         def item = new CheckboxMenuItem(action.getValue(Action.NAME) as String);
+        item.font = font;
         item.addActionListener(action);
         item.actionCommand = action.getValue(Action.ACTION_COMMAND_KEY);
         item.state = action.getValue(Action.SELECTED_KEY) as boolean;
@@ -83,11 +92,12 @@ class PopupMenuImpl extends PopupMenu implements ActionListener {
     private def getMenu(List<MenuEntry> menuEntries, MenuEntry entry) {
         def action = new MenuActionImpl(entry);
         def Menu menu = new Menu(action.getValue(Action.NAME) as String);
+        menu.font = font;
         menu.addActionListener(action);
         def path = entry.path;
         def n = path.length;
-        def v = menuEntries.findAll{it.path.length == n + 1 && it.path == entry.path + entry.name};
-        for (def e in v.sort(false, {e1, e2 -> e1.priority <=> e2.priority})) {
+        def v = menuEntries.findAll { it.path.length == n + 1 && it.path == entry.path + entry.name };
+        for (def e in v.sort(false, { e1, e2 -> e1.priority <=> e2.priority })) {
             switch (e.type) {
                 case MenuType.ITEM:
                     menu.add(getItem(e));
@@ -114,23 +124,30 @@ class PopupMenuImpl extends PopupMenu implements ActionListener {
 
     @Override
     void actionPerformed(ActionEvent e) {
-        application.frame.visible = !application.frame.visible;
+        switch (e.actionCommand) {
+            case "show_hide":
+                application.frame.visible = !application.frame.visible;
+                break;
+            case "show_log":
+                application.showLog();
+                break;
+        }
     }
 
     private void update(Menu menu) {
         for (def i = 0; i < menu.itemCount; i++) {
             def item = menu.getItem(i);
-            def action = (MenuActionImpl)item.actionListeners.find {it instanceof MenuActionImpl};
+            def action = (MenuActionImpl) item.actionListeners.find { it instanceof MenuActionImpl };
             if (action != null) {
                 action.update();
                 item.enabled = action.enabled;
                 if (item instanceof CheckboxMenuItem) {
-                    def cbi = (CheckboxMenuItem)item;
+                    def cbi = (CheckboxMenuItem) item;
                     cbi.state = action.getValue(Action.SELECTED_KEY) as boolean;
                 }
             }
             if (item instanceof Menu) {
-                update((Menu)item);
+                update((Menu) item);
             }
         }
     }
