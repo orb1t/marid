78f37a4e878ddf6f307ef273b2b6fb7acd8f87c0
Dmitry Ovchinnikov <d.ovchinnikow@gmail.com>
2014-06-28 22:18:48 +0300

    Add ConfigurationProvider

---
+++ marid-modules/marid-swing/src/main/java/org/marid/swing/forms/ConfigurationProvider.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2014 Dmitry Ovchinnikov
+ * Marid, the free data acquisition and visualization software
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.marid.swing.forms;
+
+import java.util.ServiceLoader;
+import java.util.function.Consumer;
+
+/**
+ * @author Dmitry Ovchinnikov
+ */
+public interface ConfigurationProvider {
+
+    void visitConfigurationClasses(Consumer<Class<? extends Configuration>> consumer);
+
+    static void visitConfigurations(Consumer<Class<? extends Configuration>> consumer) {
+        for (final ConfigurationProvider configurationProvider : ServiceLoader.load(ConfigurationProvider.class)) {
+            configurationProvider.visitConfigurationClasses(consumer);
+        }
+    }
+}
