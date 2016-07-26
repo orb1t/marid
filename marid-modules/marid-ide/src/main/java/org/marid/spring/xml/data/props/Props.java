d226bcb17439a6f5e026ba3195448b1c774e1a7b
Dmitry Ovchinnikov <d.ovchinnikow@gmail.com>
2016-07-26 20:59:11 +0300

    Inline AbstractProps

--- marid-modules/marid-ide/src/main/java/org/marid/spring/xml/data/props/Props.java
+++ marid-modules/marid-ide/src/main/java/org/marid/spring/xml/data/props/Props.java
@@ -18,16 +18,125 @@
 
 package org.marid.spring.xml.data.props;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import org.marid.ide.project.ProjectProfile;
+import org.marid.spring.xml.data.AbstractData;
+import org.marid.spring.xml.data.BeanLike;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+import java.lang.reflect.Executable;
+import java.util.Optional;
+import java.util.Properties;
+import java.util.stream.Stream;
+
+import static org.apache.commons.lang3.StringUtils.defaultIfBlank;
+import static org.apache.commons.lang3.StringUtils.stripToNull;
+import static org.marid.spring.xml.MaridBeanUtils.setAttr;
+import static org.marid.spring.xml.MaridBeanUtils.setProperty;
 
 /**
  * @author Dmitry Ovchinnikov.
  */
-public class Props extends AbstractProps<Props> {
+public class Props extends AbstractData<Props> implements BeanLike {
+
+    public final StringProperty id = new SimpleStringProperty(this, "id");
+    public final StringProperty valueType = new SimpleStringProperty(this, "value-type", String.class.getName());
+    public final StringProperty location = new SimpleStringProperty(this, "location");
+    public final StringProperty localOverride = new SimpleStringProperty(this, "local-override");
+    public final StringProperty ignoreResourceNotFound = new SimpleStringProperty(this, "ignore-resource-not-found");
+    public final ObservableList<PropertyEntry> entries = FXCollections.observableArrayList();
 
     @Override
-    protected Element newElement(Document document) {
-        return document.createElement("props");
+    public void writeExternal(ObjectOutput out) throws IOException {
+        out.writeUTF(defaultIfBlank(id.get(), ""));
+        out.writeUTF(defaultIfBlank(valueType.get(), ""));
+        out.writeUTF(defaultIfBlank(location.get(), ""));
+        out.writeUTF(defaultIfBlank(localOverride.get(), ""));
+        out.writeUTF(defaultIfBlank(ignoreResourceNotFound.get(), ""));
+
+        out.writeInt(entries.size());
+        for (final PropertyEntry entry : entries) {
+            out.writeObject(entry);
+        }
+    }
+
+    @Override
+    public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+        id.set(stripToNull(in.readUTF()));
+        valueType.set(stripToNull(in.readUTF()));
+        location.set(stripToNull(in.readUTF()));
+        localOverride.set(stripToNull(in.readUTF()));
+        ignoreResourceNotFound.set(stripToNull(in.readUTF()));
+
+        final int size = in.readInt();
+        for (int i = 0; i < size; i++) {
+            final String key = in.readUTF();
+            final String value = in.readUTF();
+            final PropertyEntry entry = new PropertyEntry();
+            entry.key.set(key);
+            entry.value.set(value);
+            entries.add(entry);
+        }
+    }
+
+    @Override
+    public Stream<? extends Executable> getConstructors(ProjectProfile profile) {
+        return Stream.empty();
+    }
+
+    @Override
+    public Optional<Class<?>> getClass(ProjectProfile profile) {
+        return Optional.of(Properties.class);
+    }
+
+    @Override
+    public void updateBeanData(ProjectProfile profile) {
+    }
+
+    @Override
+    public StringProperty nameProperty() {
+        return id;
+    }
+
+    @Override
+    public void save(Node node, Document document) {
+        final Element element = document.createElement("props");
+        node.appendChild(element);
+
+        setAttr(id, element);
+        setAttr(ignoreResourceNotFound, element);
+        setAttr(localOverride, element);
+        setAttr(valueType, element);
+        setAttr(location, element);
+
+        entries.forEach(entry -> entry.save(element, document));
+    }
+
+    @Override
+    public void load(Node node, Document document) {
+        final Element element = (Element) node;
+
+        setProperty(id, element);
+        setProperty(valueType, element);
+        setProperty(location, element);
+        setProperty(localOverride, element);
+        setProperty(ignoreResourceNotFound, element);
+
+        final NodeList children = element.getElementsByTagName("prop");
+        for (int i = 0; i < children.getLength(); i++) {
+            final Element e = (Element) children.item(i);
+            final PropertyEntry entry = new PropertyEntry();
+            entry.load(e, document);
+            entries.add(entry);
+        }
     }
 }
