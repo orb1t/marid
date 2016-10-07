0acda2e02a2eed458d81a7a52970ff3a1b8abe83
Dmitry Ovchinnikov <d.ovchinnikow@gmail.com>
2016-10-07 22:35:17 +0300

    Factory beans cascade creation

--- marid-modules/marid-fx/src/main/java/org/marid/jfx/util/MaridCollections.java
+++ marid-modules/marid-fx/src/main/java/org/marid/jfx/util/MaridCollections.java
@@ -20,6 +20,7 @@ package org.marid.jfx.util;
 
 import javafx.beans.Observable;
 import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 
 import java.util.List;
@@ -30,10 +31,26 @@ import java.util.List;
 public interface MaridCollections {
 
     static <E extends Observable> ObservableList<E> list(List<E> list) {
-        return FXCollections.observableList(list, e -> new Observable[] {e});
+        return fix(FXCollections.observableList(list, e -> new Observable[] {e}));
     }
 
     static <E extends Observable> ObservableList<E> list() {
-        return FXCollections.observableArrayList(e -> new Observable[] {e});
+        return fix(FXCollections.observableArrayList(e -> new Observable[] {e}));
+    }
+
+    static <E extends Observable> ObservableList<E> fix(ObservableList<E> list) {
+        final ListChangeListener<E> changeListener = c -> {
+            while (c.next()) {
+                if (c.wasUpdated()) {
+                    for (int i = c.getFrom(); i < c.getTo(); i++) {
+                        final E old = list.get(i);
+                        list.set(i, null);
+                        list.set(i, old);
+                    }
+                }
+            }
+        };
+        list.addListener(changeListener);
+        return list;
     }
 }
