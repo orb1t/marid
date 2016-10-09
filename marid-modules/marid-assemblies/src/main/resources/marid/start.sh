25bcc88a9a134a618e69fc9afd56db2b6861ffa3
Dmitry Ovchinnikov <d.ovchinnikow@gmail.com>
2016-10-09 08:25:51 +0300

    Fix assemblies

--- marid-modules/marid-assemblies/src/main/resources/marid/start.sh
+++ marid-modules/marid-assemblies/src/main/resources/marid/start.sh
@@ -4,4 +4,4 @@ cd "$(dirname $0)"
 
 JAVA_CMD="$JAVA_HOME/bin/java"
 
-$JAVA_CMD -jar ${artifactId}.${project.packaging} $@ Init > "logs/${project.artifactId}.log" 2>&1 &
+$JAVA_CMD -jar lib/${project.build.finalName}.jar > "logs/${project.artifactId}.log" 2>&1 &
