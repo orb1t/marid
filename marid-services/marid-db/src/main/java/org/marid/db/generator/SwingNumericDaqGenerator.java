9c1ac5aadaf06b6cf033649188f801e265bd0de4
Dmitry Ovchinnikov <d.ovchinnikow@gmail.com>
2016-10-12 22:22:25 +0300

    Fix shutdown issues

--- marid-services/marid-db/src/main/java/org/marid/db/generator/SwingNumericDaqGenerator.java
+++ marid-services/marid-db/src/main/java/org/marid/db/generator/SwingNumericDaqGenerator.java
@@ -27,6 +27,12 @@ import org.springframework.context.event.ContextClosedEvent;
 import javax.swing.*;
 import javax.swing.plaf.nimbus.NimbusLookAndFeel;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Stream;
 
 import static java.awt.GridBagConstraints.*;
 import static javax.swing.ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER;
@@ -61,22 +67,97 @@ public class SwingNumericDaqGenerator implements LogSupport {
         });
     }
 
+    private static ImageIcon icon(String url) {
+        try {
+            return new ImageIcon(new URL(url));
+        } catch (Exception x) {
+            return null;
+        }
+    }
+
     private class Frame extends JFrame {
 
         private final GridBagLayout layout = new GridBagLayout();
         private final GridBagConstraints constraints = new GridBagConstraints(
                 REMAINDER, RELATIVE, 1, 1, 1.0, 0.0, BASELINE, HORIZONTAL, new Insets(5, 5, 5, 5), 0, 0);
         private final JPanel panel = new JPanel();
+        private final JSpinner periodSpinner = new JSpinner(new SpinnerNumberModel(1000, 100, 60000, 100));
+        private final Map<String, Action> actionMap = new HashMap<>();
+        private final Timer timer = new Timer((int) periodSpinner.getValue(), e -> {
+
+        });
 
         private Frame() {
             super(s("Swing numeric DAQ generator"));
             setDefaultCloseOperation(DISPOSE_ON_CLOSE);
             setJMenuBar(new JMenuBar());
-            panel.setPreferredSize(new Dimension(800, 600));
+            setPreferredSize(new Dimension(800, 600));
             panel.setLayout(layout);
             add(new JScrollPane(panel, VERTICAL_SCROLLBAR_AS_NEEDED, HORIZONTAL_SCROLLBAR_NEVER));
+            add(toolBar(), BorderLayout.NORTH);
             pack();
             setLocationRelativeTo(null);
+            periodSpinner.addChangeListener(e -> timer.setDelay((int) periodSpinner.getValue()));
+        }
+
+        private JToolBar toolBar() {
+            final JToolBar toolBar = new JToolBar();
+            toolBar.setFloatable(false);
+            add(toolBar, "Add tag", "awicons/vista-artistic/24/add-icon.png", e -> {
+                panel.add(new TagPanel(), constraints);
+                panel.validate();
+                panel.getParent().validate();
+            });
+            toolBar.addSeparator();
+            add(toolBar, "Load", "oxygen-icons.org/oxygen/24/Actions-document-open-icon.png", e -> {
+            });
+            add(toolBar, "Save", "oxygen-icons.org/oxygen/24/Actions-document-save-icon.png", e -> {
+            });
+            toolBar.addSeparator();
+            add(toolBar, "Run", "oxygen-icons.org/oxygen/24/Actions-media-playback-start-icon.png", e -> {
+                actionMap.get("Run").setEnabled(false);
+                actionMap.get("Stop").setEnabled(true);
+                timer.start();
+            });
+            add(toolBar, "Stop", "oxygen-icons.org/oxygen/24/Actions-media-playback-stop-icon.png", e -> {
+                actionMap.get("Run").setEnabled(true);
+                actionMap.get("Stop").setEnabled(false);
+                timer.stop();
+            }).getAction().setEnabled(false);
+            toolBar.addSeparator();
+            toolBar.add(periodSpinner);
+            Stream.of(toolBar.getComponents()).forEach(c -> c.setFocusable(false));
+            return toolBar;
+        }
+
+        private JButton add(JToolBar toolBar, String label, String path, ActionListener listener) {
+            final String text = s(label);
+            final AbstractAction action = new AbstractAction(text, icon("http://icons.iconarchive.com/icons/" + path)) {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    listener.actionPerformed(e);
+                }
+            };
+            actionMap.put(label, action);
+            action.putValue(Action.SHORT_DESCRIPTION, text);
+            return toolBar.add(action);
+        }
+    }
+
+    private class TagPanel extends JPanel {
+
+        private final JTextField max = new JTextField("1.0");
+        private final JTextField min = new JTextField("0.0");
+        private final JTextField tick = new JTextField("0.01");
+
+        private TagPanel() {
+            super(new FlowLayout());
+            add(new JLabel(s("Min") + ": "));
+            add(min);
+            add(new JLabel(s("Max") + ": "));
+            add(max);
+            add(new JLabel(s("Tick") + ": "));
+            add(tick);
         }
     }
 }
