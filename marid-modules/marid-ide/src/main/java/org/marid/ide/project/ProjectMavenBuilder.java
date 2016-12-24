b8b415a43fcdd65cc46c293192ad3aea94f3b51d
Dmitry Ovchinnikov <d.ovchinnikow@gmail.com>
2016-12-24 13:20:42 +0300

    ProjectMavenBuilder: do not create temp directory

--- marid-modules/marid-ide/src/main/java/org/marid/ide/project/ProjectMavenBuilder.java
+++ marid-modules/marid-ide/src/main/java/org/marid/ide/project/ProjectMavenBuilder.java
@@ -18,11 +18,12 @@
 
 package org.marid.ide.project;
 
+import com.sun.net.httpserver.HttpServer;
 import javafx.application.Platform;
-import org.codehaus.plexus.util.FileUtils;
 import org.marid.Ide;
 import org.marid.ide.common.IdeValues;
 import org.marid.jfx.action.FxAction;
+import org.marid.logging.LogSupport;
 import org.marid.maven.ProjectBuilder;
 import org.marid.maven.ProjectBuilderFactory;
 import org.springframework.beans.factory.ObjectProvider;
@@ -30,53 +31,74 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PreDestroy;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.URL;
 import java.net.URLClassLoader;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
 import java.util.*;
 import java.util.function.Consumer;
 import java.util.logging.LogRecord;
+import java.util.stream.Collectors;
 import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
+import java.util.zip.ZipFile;
+
+import static java.net.HttpURLConnection.HTTP_OK;
+import static java.util.Objects.requireNonNull;
 
 /**
  * @author Dmitry Ovchinnikov
  */
 @Component
-public class ProjectMavenBuilder {
+public class ProjectMavenBuilder implements LogSupport {
 
-    private final Path tempDirectory;
     private final URLClassLoader classLoader;
     private final ObjectProvider<FxAction> projectBuildAction;
+    private final HttpServer httpServer;
+    private final ZipFile zip;
 
     @Autowired
     public ProjectMavenBuilder(IdeValues ideValues, ObjectProvider<FxAction> projectBuildAction) throws Exception {
         this.projectBuildAction = projectBuildAction;
-        this.tempDirectory = Files.createTempDirectory("projectBuilder");
         final String resource = String.format("marid-maven-%s.zip", ideValues.implementationVersion);
-        final URL url = Objects.requireNonNull(Ide.classLoader.getResource(resource), "marid-maven artifact is not found");
-        try (final ZipInputStream zipInputStream = new ZipInputStream(url.openStream(), StandardCharsets.UTF_8)) {
-            final List<URL> urls = new ArrayList<>();
-            for (ZipEntry entry = zipInputStream.getNextEntry(); entry != null; entry = zipInputStream.getNextEntry()) {
-                if (entry.getName().endsWith(".jar")) {
-                    final Path target = tempDirectory.resolve(entry.getName());
-                    Files.copy(zipInputStream, target, StandardCopyOption.REPLACE_EXISTING);
-                    urls.add(target.toUri().toURL());
-                }
-                zipInputStream.closeEntry();
-            }
-            classLoader = URLClassLoader.newInstance(urls.toArray(new URL[urls.size()]));
+        final URL baseUrl = requireNonNull(Ide.classLoader.getResource(resource), "marid-maven is not found");
+        zip = new ZipFile(new File(baseUrl.toURI()));
+        httpServer = HttpServer.create(new InetSocketAddress(0), 5);
+        final Set<String> jars = zip.stream()
+                .filter(e -> e.getName().endsWith(".jar"))
+                .peek(e -> httpServer.createContext("/" + e.getName(), ex -> {
+                    ex.sendResponseHeaders(HTTP_OK, e.getSize());
+                    try (final OutputStream os = ex.getResponseBody(); final InputStream is = zip.getInputStream(e)) {
+                        final byte[] buffer = new byte[(int) e.getSize()];
+                        for (int n = is.read(buffer); n >= 0; n = is.read(buffer)) {
+                            os.write(buffer, 0, n);
+                        }
+                    } catch (Exception x) {
+                        log(WARNING, "Transfer error", x);
+                    }
+                }))
+                .map(ZipEntry::getName)
+                .collect(Collectors.toSet());
+        httpServer.start();
+        final List<URL> urls = new ArrayList<>(jars.size());
+        final InetAddress localhost = InetAddress.getByAddress(InetAddress.getLocalHost().getAddress());
+        final InetSocketAddress address = new InetSocketAddress(localhost, httpServer.getAddress().getPort());
+        for (final String jar : jars) {
+            urls.add(new URL("http:/" + address + "/" + jar));
         }
+        classLoader = new URLClassLoader(urls.toArray(new URL[urls.size()]));
     }
 
     @PreDestroy
     private void destroy() throws IOException {
-        classLoader.close();
-        FileUtils.deleteDirectory(tempDirectory.toFile());
+        try (final ZipFile z = zip; final URLClassLoader u = classLoader) {
+            log(INFO, "Closing {0} and {1}", z, u);
+        } finally {
+            httpServer.stop(1);
+        }
     }
 
     public Thread build(ProjectProfile profile, Consumer<Map<String, Object>> resultConsumer, Consumer<LogRecord> logConsumer) {
