975dcf4705314cc5aff028b0486768a82467da0d
Dmitry Ovchinnikov <d.ovchinnikow@gmail.com>
2017-10-08 18:05:00 +0300

    menu bar

--- marid-modules/marid-runtime/src/test/java/org/marid/runtime/expression/ExpressionSerializationTest.java
+++ marid-modules/marid-runtime/src/test/java/org/marid/runtime/expression/ExpressionSerializationTest.java
@@ -22,7 +22,10 @@
 package org.marid.runtime.expression;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.marid.io.Xmls;
+import org.marid.misc.Casts;
 
 import javax.xml.transform.stream.StreamResult;
 import java.io.IOException;
@@ -30,42 +33,86 @@ import java.io.StringReader;
 import java.io.StringWriter;
 import java.io.UncheckedIOException;
 import java.util.function.BiConsumer;
-import java.util.function.Consumer;
+import java.util.stream.Stream;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ExpressionSerializationTest {
 
     @Test
-    void testNull() throws Exception {
-        final NullExpr expr = new NullExpr();
-
-        final StringWriter writer = new StringWriter();
-
-        try (writer) {
-            Xmls.writeFormatted(expr::to, new StreamResult(writer));
-        }
-
-        final StringReader reader = new StringReader(writer.toString());
-
-        try (reader) {
-            final Expression expression = Xmls.read(reader, NullExpr.NULL::from);
-
-            assertTrue(expression instanceof NullExpression);
-        }
+    void testNull() {
+        check(new NullExpr(), (e, a) -> {});
     }
 
-    private <T extends Expression> void check(Class<T> type, Consumer<T> configurator, BiConsumer<T, T> checker){
-        final T expected;
-        try {
-            expected = type.getConstructor().newInstance();
-        } catch (ReflectiveOperationException x) {
-            throw new IllegalStateException(x);
-        }
-        configurator.accept(expected);
+    private static Stream<? extends ValueExpression> valueClasses() {
+        return Stream.of(
+                new BooleanExpr("x"),
+                new IntegerExpr("x"),
+                new LongExpr("x"),
+                new ShortExpr("x"),
+                new ByteExpr("x"),
+                new CharExpr("x"),
+                new DoubleExpr("x"),
+                new FloatExpr("x")
+        );
+    }
 
+    @ParameterizedTest
+    @MethodSource("valueClasses")
+    void testValues(ValueExpr expr) {
+        check(expr, (e, a) -> assertEquals(e.getValue(), a.getValue()));
+    }
+
+    @Test
+    void testString() {
+        check(new StringExpr("x"), (e, a) -> assertEquals(e.getValue(), a.getValue()));
+    }
+
+    @Test
+    void testRef() {
+        check(new RefExpr("z"), (e, a) -> assertEquals(e.getReference(), a.getReference()));
+    }
+
+    @Test
+    void testMethodCall() {
+        check(new MethodCallExpr(
+                        new BooleanExpr("x"),
+                        "x",
+                        new MethodCallStaticExpr(
+                                new ConstructorCallExpr(NullExpr.NULL, ThisExpr.THIS),
+                                "v",
+                                new IntegerExpr("0"),
+                                new LongExpr("1")
+                        ),
+                        new RefExpr("u")),
+                (e, a) -> {
+                    final BooleanExpr expectedTarget = (BooleanExpr) e.getTarget();
+                    final BooleanExpr actualTarget = (BooleanExpr) a.getTarget();
+                    assertEquals(expectedTarget.getValue(), actualTarget.getValue());
+
+                    assertEquals(e.getMethod(), a.getMethod());
+
+                    final MethodCallStaticExpr expectedArg0 = (MethodCallStaticExpr) e.getArgs().get(0);
+                    final MethodCallStaticExpr actualArg0 = (MethodCallStaticExpr) a.getArgs().get(0);
+
+                    assertEquals(expectedArg0.getMethod(), actualArg0.getMethod());
+
+                    final ConstructorCallExpr actualArg0Target = (ConstructorCallExpr) actualArg0.getTarget();
+
+                    assertTrue(actualArg0Target.getTarget() instanceof NullExpression);
+                    assertTrue(actualArg0Target.getArgs().get(0) instanceof ThisExpression);
+
+                    final IntegerExpr arg0 = (IntegerExpr) actualArg0.getArgs().get(0);
+                    final LongExpr arg1 = (LongExpr) actualArg0.getArgs().get(1);
+
+                    assertEquals("0", arg0.getValue());
+                    assertEquals("1", arg1.getValue());
+                });
+    }
+
+    private <T extends Expression> void check(T expected, BiConsumer<T, T> checker) {
         final StringWriter writer = new StringWriter();
-
         try (writer) {
             Xmls.writeFormatted(expected::to, new StreamResult(writer));
         } catch (IOException x) {
@@ -73,11 +120,12 @@ class ExpressionSerializationTest {
         }
 
         final StringReader reader = new StringReader(writer.toString());
-
         try (reader) {
             final Expression expression = Xmls.read(reader, NullExpr.NULL::from);
 
-            assertTrue(expression instanceof NullExpression);
+            final Class<T> type = Casts.cast(expected.getClass());
+            assertTrue(type.isInstance(expression));
+            checker.accept(expected, type.cast(expression));
         }
     }
 }
