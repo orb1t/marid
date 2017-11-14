14752d6ca23581b3c93b529a4361f91583c0a9da
Dmitry Ovchinnikov <d.ovchinnikow@gmail.com>
2017-11-14 19:05:54 +0300

    Remove guava completely

--- marid-modules/marid-types/src/main/java/org/marid/types/TypeResolver.java
+++ marid-modules/marid-types/src/main/java/org/marid/types/TypeResolver.java
@@ -21,26 +21,57 @@
 
 package org.marid.types;
 
+import org.apache.commons.lang3.reflect.TypeUtils;
+
 import javax.annotation.Nonnull;
+import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.lang.reflect.TypeVariable;
+import java.lang.reflect.WildcardType;
 import java.util.HashMap;
-import java.util.stream.Stream;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
 
-import static java.util.function.Function.identity;
-import static java.util.stream.Collectors.toMap;
-import static java.util.stream.Stream.concat;
-import static java.util.stream.Stream.of;
+import static org.apache.commons.lang3.reflect.TypeUtils.getArrayComponentType;
+import static org.marid.types.TypeUtil.boxed;
 
 public class TypeResolver {
 
   private final Class<?> type;
-  private final HashMap<TypeVariable<?>, Type> mapping;
+  private final HashMap<TypeVariable<?>, HashSet<Type>> mapping = new HashMap<>();
 
   public TypeResolver(@Nonnull Class<?> type) {
     this.type = type;
-    this.mapping = concat(of(type), concat(of(type.getMethods()), of(type.getConstructors())))
-        .flatMap(d -> Stream.of(d.getTypeParameters()))
-        .collect(toMap(identity(), identity(), (v1, v2) -> v2, HashMap::new));
+  }
+
+  public void where(@Nonnull Type formal, @Nonnull Type actual) {
+    where(new HashSet<>(), formal, actual);
+  }
+
+  private void where(@Nonnull HashSet<Type> passed, @Nonnull Type formal, @Nonnull Type actual) {
+    if (formal instanceof TypeVariable<?>) {
+      mapping.computeIfAbsent((TypeVariable<?>) formal, k -> new HashSet<>()).add(actual);
+      for (final Type bound : ((TypeVariable<?>) formal).getBounds()) {
+        where(passed, bound, actual);
+      }
+    } else if (passed.add(formal)) {
+      if (TypeUtils.isArrayType(formal) && TypeUtils.isArrayType(actual)) {
+        where(passed, getArrayComponentType(formal), getArrayComponentType(actual));
+      } else if (formal instanceof ParameterizedType) {
+        final Class<?> formalRaw = TypeUtil.getRaw(formal);
+        final Class<?> actualRaw = TypeUtil.getRaw(actual);
+        if (formalRaw.isAssignableFrom(actualRaw)) {
+          TypeUtils.getTypeArguments(actual, formalRaw).forEach((v, t) -> {
+            final HashSet<Type> types = mapping.computeIfAbsent(v, k -> new LinkedHashSet<>());
+            types.add(boxed(t));
+          });
+        }
+      } else if (formal instanceof WildcardType) {
+        final WildcardType wildcardType = (WildcardType) formal;
+        for (final Type bound : wildcardType.getUpperBounds()) {
+          where(passed, bound, actual);
+        }
+      }
+    }
   }
 }
