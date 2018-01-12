34de5d27939dbc213142432ec54289154b3db174
Dmitry Ovchinnikov <d.ovchinnikow@gmail.com>
2018-01-12 20:19:53 +0300

    Migrate to marid-intellij-annotations

--- marid-modules/marid-site/src/main/java/org/marid/site/LoginUI.java
+++ marid-modules/marid-site/src/main/java/org/marid/site/LoginUI.java
@@ -2,19 +2,27 @@ package org.marid.site;
 
 import com.vaadin.annotations.Theme;
 import com.vaadin.cdi.CDIUI;
+import com.vaadin.cdi.CDIViewProvider;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
+import javax.inject.Inject;
+
 @Theme("runo")
-@CDIUI
-public class SiteUI extends UI {
+@CDIUI("")
+public class LoginUI extends UI {
+
+  @Inject
+  private CDIViewProvider viewProvider;
 
   @Override
   protected void init(VaadinRequest vaadinRequest) {
     final VerticalLayout layout = new VerticalLayout();
     layout.addComponent(new Button("Add"));
+
+    System.out.println(viewProvider);
 
     setContent(layout);
   }
