f23c4fc550538a3107a41770ef8a8f630e297c7c
Dmitry Ovchinnikov <d.ovchinnikow@gmail.com>
2018-01-16 21:36:45 +0300

    Migrate to marid-intellij-annotations

--- marid-modules/marid-site/src/main/java/org/marid/site/servlets/ErrorServlet.java
+++ marid-modules/marid-site/src/main/java/org/marid/site/servlets/ErrorServlet.java
@@ -19,32 +19,11 @@
  * #L%
  */
 
-package org.marid.site;
+package org.marid.site.servlets;
 
-import com.vaadin.annotations.Theme;
-import com.vaadin.cdi.CDIUI;
-import com.vaadin.cdi.CDIViewProvider;
-import com.vaadin.server.VaadinRequest;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.UI;
-import com.vaadin.ui.VerticalLayout;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 
-import javax.inject.Inject;
-
-@Theme("marid")
-@CDIUI("")
-public class LoginUI extends UI {
-
-  @Inject
-  private CDIViewProvider viewProvider;
-
-  @Override
-  protected void init(VaadinRequest vaadinRequest) {
-    final VerticalLayout layout = new VerticalLayout();
-    layout.addComponent(new Button("Add"));
-
-    System.out.println(viewProvider);
-
-    setContent(layout);
-  }
+@WebServlet(urlPatterns = {"/error"})
+public class ErrorServlet extends HttpServlet {
 }
