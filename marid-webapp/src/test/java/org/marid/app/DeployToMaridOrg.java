04e9111e9de57fff6a1b739455761424cb196f22
Dmitry Ovchinnikov <d.ovchinnikow@gmail.com>
2018-03-18 18:35:09 +0300

    Users

---
+++ marid-webapp/src/test/java/org/marid/app/DeployToMaridOrg.java
@@ -0,0 +1,49 @@
+/*-
+ * #%L
+ * marid-webapp
+ * %%
+ * Copyright (C) 2012 - 2018 MARID software development group
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+
+package org.marid.app;
+
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.CodeSource;
+import java.security.ProtectionDomain;
+
+public class DeployToMaridOrg {
+
+  public static void main(String... args) throws Throwable {
+    final ProtectionDomain protectionDomain = DeployToMaridOrg.class.getProtectionDomain();
+    final CodeSource codeSource = protectionDomain.getCodeSource();
+    final URL location = codeSource.getLocation();
+    final Path path = Paths.get(location.toURI()).getParent().getParent();
+    final Process process = new ProcessBuilder(
+        "mvn",
+        "wagon:sshexec@stop",
+        "wagon:upload-single@deploy",
+        "wagon:sshexec@start"
+    )
+        .inheritIO()
+        .directory(path.toFile())
+        .start();
+    final int result = process.waitFor();
+    System.out.println(result);
+  }
+}
