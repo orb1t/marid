9c3de805e9dc153e712a4e0e270a666b9192dab0
Dmitry Ovchinnikov <d.ovchinnikow@gmail.com>
2018-03-18 19:01:05 +0300

    Users

---
+++ marid-webapp/src/test/java/org/marid/app/BuildAll.java
@@ -0,0 +1,33 @@
+/*-
+ * #%L
+ * marid-webapp
+ * %%
+ * Copyright (C) 2012 - 2018 MARID software development group
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+
+package org.marid.app;
+
+public class BuildAll {
+
+  public static void main(String... args) throws Throwable {
+    final Process process = new ProcessBuilder("mvn", "-B", "-DskipTests", "clean", "install")
+        .inheritIO()
+        .start();
+    final int result = process.waitFor();
+    System.out.println(result);
+  }
+}
